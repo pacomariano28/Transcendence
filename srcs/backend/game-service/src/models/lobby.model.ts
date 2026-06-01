3a48a41c131f1c86db615b9ec4ea7ce927446351
Seilán Moore Arnanz <145937342+seilanmoore@users.noreply.github.com>
2026-06-01 12:26:18 +0200

    Implement playlist retrieval and enhance lobby management (#33)
    * feature(game-service): implement  playlist retrieval
    
    * feature(game-service): enhance lobby management with join and ready functionality
    
    ---------
    
    Co-authored-by: pacomariano28 <pacomariano28@gmail.com>

--- srcs/backend/game-service/src/models/lobby.model.ts
+++ srcs/backend/game-service/src/models/lobby.model.ts
@@ -1,16 +1,29 @@
-// /srcs/backend/game-service/src/models/lobby.model.ts
+export interface Player {
+  userId: string;
+  username: string;
+  score: number;
+  ready: boolean;
+}
 
 export interface PlaylistItem {
   trackId: string;
   fileName: string;
 }
 
+export interface RoundStatus {
+  active: boolean;
+  winner: string | null;
+  tries: number | null;
+}
+
 export interface Lobby {
   lobbyId: string;
-  name: string;
-  players: any[]; // Puedes tipar mejor luego
-  state: "waiting" | "in_progress" | "finished";
-  createdAt: Date;
+  state: "waiting" | "playing" | "finished";
+  hostUserId: string;
+  createdAt: string;
+  players: Player[];
   playlist: PlaylistItem[];
   currentRound: number;
+  rounds: RoundStatus[];
+  maxPlayers: number; // <-- nuevo campo
 }
