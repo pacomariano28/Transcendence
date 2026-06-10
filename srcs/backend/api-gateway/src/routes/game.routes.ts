468d6e1bb672bf507ac0698533d5c2080a8858f9
smoore-a <smoore-a@student.42malaga.com>
2026-06-10 12:03:52 +0200

    refactor(gateway): handle socket and api rest requests separely

--- srcs/backend/api-gateway/src/routes/game.routes.ts
+++ srcs/backend/api-gateway/src/routes/game.routes.ts
@@ -1,16 +1,37 @@
 import { createProxyMiddleware, fixRequestBody } from "http-proxy-middleware";
 import { Router } from "express";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router: Router = Router();
 
 const GAME_SERVICE_URL =
   process.env.GAME_SERVICE_URL || "http://game-service:4001";
 
-export const gameProxy = createProxyMiddleware({
+export const socketProxy = createProxyMiddleware({
   target: GAME_SERVICE_URL,
   changeOrigin: true,
   ws: true,
-  pathRewrite: { "^/api/game": "" },
+  logger: console,
+  pathRewrite: { "^/socket.io": "/socket.io" },
+  on: {
+    proxyReq: (proxyReq, req) => {
+      console.log(`[PROXY OUT] Proxying to: ${proxyReq.path}`);
+      fixRequestBody(proxyReq, req);
+    },
+    proxyRes: (proxyRes, _req) => {
+      void _req;
+      // <--- 3. Log de lo que el game-service contesta
+      console.log(
+        `[PROXY RES] Target responded with status: ${proxyRes.statusCode}`,
+      );
+    },
+  },
+});
+
+const proxyOptions = createProxyMiddleware({
+  target: GAME_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: { "^/api/game": "/" },
   on: {
     proxyReq: (proxyReq, req) => {
       fixRequestBody(proxyReq, req);
@@ -18,6 +39,6 @@ export const gameProxy = createProxyMiddleware({
   },
 });
 
-router.use("/", gameProxy);
+router.get("/state", requireAuth, proxyOptions);
 
 export default router;
