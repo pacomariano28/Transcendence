39391d1c5fd412d68ad9a1d07d7dd972bc6681d1
svetameanssun <svetameanssun@gmail.com>
2026-06-10 17:11:51 +0200

    ci(migration): added migrations

---
+++ srcs/backend/playlist-service/prisma/migrations/20260610113143_init_playlist/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Song" (
+    "id" TEXT NOT NULL,
+    "trackId" TEXT NOT NULL,
+    "fileName" TEXT NOT NULL,
+    "used" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Song_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Song_fileName_key" ON "Song"("fileName");
