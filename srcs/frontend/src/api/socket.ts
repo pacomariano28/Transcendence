6bb72887bc2af8e9fbfd84e31c696611f54b9870
smoore-a <smoore-a@student.42malaga.com>
2026-06-10 12:08:13 +0200

    refactor(socket-client): change socket path

--- srcs/frontend/src/api/socket.ts
+++ srcs/frontend/src/api/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_BASE_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(SOCKET_BASE_URL, {
-  path: "/api/game/socket.io",
+  path: "/socket.io",
   autoConnect: false,
   withCredentials: true,
 });
