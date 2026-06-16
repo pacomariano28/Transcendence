df68d4abee0dea4c589035a1ea1892ab50a090a7
svetameanssun <svetameanssun@gmail.com>
2026-06-16 13:53:22 +0200

    refactor(script):Grant permissions to migrations in postgres

--- infra/postgres/init-multiple-databases.sh
+++ infra/postgres/init-multiple-databases.sh
@@ -11,6 +11,10 @@ function create_user_and_database() {
         CREATE DATABASE $database;
         GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
+
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$database" <<-EOSQL
+        GRANT ALL ON SCHEMA public TO $database;
+EOSQL
 }
 
 # Check if the variable exists in docker-compose and go through the given list of databases
