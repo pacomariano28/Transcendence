257b87cd03a17064acec9877cfcf91330d9384c2
Francisco Mariano Ortiz <pacomariano28@gmail.com>
2026-07-08 12:46:34 +0200

    feature/downloader-script: add and upgrade downloader.sh (#66)

--- srcs/backend/playlist-service/prisma/seed.ts
+++ srcs/backend/playlist-service/prisma/seed.ts
@@ -8,203 +8,191 @@ async function main() {
   await prisma.song.createMany({
     data: [
       {
-        trackId: "3QFInJAm9eyaho5vBzxInN",
+        trackId: "3GvDCvfTvXB4cp8wwh7Y3k",
         fileName: "preview_001.mp3",
       },
       {
-        trackId: "760IJcunfpbkm6sHbMmyyj",
+        trackId: "3ivnBohmIbZFrtzGkSqUtN",
         fileName: "preview_002.mp3",
       },
       {
-        trackId: "5TRPicyLGbAF2LGBFbHGvO",
+        trackId: "5V1YyUMhty1NCO3zVePAtR",
         fileName: "preview_003.mp3",
       },
       {
-        trackId: "68Dni7IE4VyPkTOH9mRWHr",
-        fileName: "preview_004.mp3",
-      },
-      {
-        trackId: "51EC3I1nQXpec4gDk0mQyP",
+        trackId: "0isjtJAFQqeerWeecGmc1u",
         fileName: "preview_005.mp3",
       },
       {
-        trackId: "2N3U8uTYhjX2KwGT0pf5x0",
+        trackId: "4Z6pPinNzkGtbgAlE88263",
         fileName: "preview_006.mp3",
       },
       {
-        trackId: "60PAzFNW3vAiAiVK6DRJfB",
+        trackId: "2gEthirHCqqUybpq7HkNY7",
         fileName: "preview_007.mp3",
       },
       {
-        trackId: "30QR0ndUdiiMQMA9g1PGCm",
+        trackId: "4a2KalFjqxANM6WAfR3sYJ",
         fileName: "preview_008.mp3",
       },
       {
-        trackId: "3yk7PJnryiJ8mAPqsrujzf",
+        trackId: "763beEZikxBlbwHvdlCWnh",
         fileName: "preview_009.mp3",
       },
       {
-        trackId: "2axiRrUWmlFUKmPzDsjjzg",
+        trackId: "7x011H8oZwGYBuXQQfGpu4",
         fileName: "preview_010.mp3",
       },
       {
-        trackId: "50a8bKqlwDEqeiEknrzkTO",
+        trackId: "6YfQ2Ev74hY6Tsw5q8y2XX",
         fileName: "preview_011.mp3",
       },
       {
-        trackId: "6IZvVAP7VPPnsGX6bvgkqg",
+        trackId: "64yCeFRhL4hTNik8J3Un6D",
         fileName: "preview_012.mp3",
       },
       {
-        trackId: "58k32my5lKofeZRtIvBDg9",
+        trackId: "35hYk23wFUOsHoQfljbz80",
         fileName: "preview_013.mp3",
       },
       {
-        trackId: "0fv2KH6hac06J86hBUTcSf",
+        trackId: "7zxw2muWhobnpwEdABBO83",
         fileName: "preview_014.mp3",
       },
       {
-        trackId: "4aOOExMBUyxKnEYb39SrTg",
+        trackId: "16RcB6ae7pNeCQVIrPuytu",
         fileName: "preview_015.mp3",
       },
       {
-        trackId: "5bJ1DrEM4hNCafcDd1oxHx",
+        trackId: "1TWAUx1pXxA5XxJmHMGT0b",
         fileName: "preview_016.mp3",
       },
       {
-        trackId: "3s7MCdXyWmwjdcWh7GWXas",
+        trackId: "2TrtWQKmcdoSGjsJDc3N64",
         fileName: "preview_017.mp3",
       },
       {
-        trackId: "393MDhe62s8hbH8ETrlxe5",
+        trackId: "4YZhsgOBQtKvxZWjVuJD9H",
         fileName: "preview_018.mp3",
       },
       {
-        trackId: "6LyAwkJsHlW7RQ8S1cYAtM",
+        trackId: "0UxAVbd9Fjp7O2SvzV2fJJ",
         fileName: "preview_019.mp3",
       },
       {
-        trackId: "5n4FTCMefvyKUjeWumdaWv",
+        trackId: "3SK45LddxlEkzI8OWO9Eyo",
         fileName: "preview_020.mp3",
       },
       {
-        trackId: "4tqcoej1zPvwePZCzuAjJd",
+        trackId: "0EsMceg5ksKflUYXt2WMne",
         fileName: "preview_021.mp3",
       },
       {
-        trackId: "3vQ4T78TTMOjQXGfXVKQJo",
+        trackId: "44aqOr4450KjyEZuW9Ekpm",
         fileName: "preview_022.mp3",
       },
       {
-        trackId: "0zO8ctW0UiuOefR87OeJOZ",
+        trackId: "712KzUVmtBeFXgJhbMJY5o",
         fileName: "preview_023.mp3",
       },
       {
-        trackId: "0uxSUdBrJy9Un0EYoBowng",
+        trackId: "6m8DnCqkHlUfcjXcjOmOu2",
         fileName: "preview_024.mp3",
       },
       {
-        trackId: "4lH6nENd1y81jp7Yt9lTBX",
+        trackId: "0sVAsg0GP4JZHKn1Ohsiw0",
         fileName: "preview_025.mp3",
       },
       {
-        trackId: "7J2gyNghNTzl4EsLhXp01Q",
+        trackId: "2UP28r5tYcvHi7ZJIK3DjG",
         fileName: "preview_026.mp3",
       },
       {
-        trackId: "1Is8hGpkGMiePASAxBluxM",
-        fileName: "preview_027.mp3",
-      },
-      {
-        trackId: "7rbECVPkY5UODxoOUVKZnA",
+        trackId: "22hmwgik7o71IiNVsCMDVF",
         fileName: "preview_028.mp3",
       },
       {
-        trackId: "0m0GzwCkfuFcxOlLjgpudo",
+        trackId: "0yK1nUASmGNYKUH5qpXeOY",
         fileName: "preview_029.mp3",
       },
       {
-        trackId: "11kDth1aKUEUMq9r1pqyds",
+        trackId: "3X7BShvdMM3posr8IDK0J5",
         fileName: "preview_030.mp3",
       },
       {
-        trackId: "29TPjc8wxfz4XMn21O7VsZ",
+        trackId: "0ojp3rI6wfc6GjATk2SEir",
         fileName: "preview_031.mp3",
       },
       {
-        trackId: "7ycWLEP1GsNjVvcjawXz3z",
+        trackId: "5Vv9KbtAN8XNESS8XH1sPt",
         fileName: "preview_032.mp3",
       },
       {
-        trackId: "0I3q5fE6wg7LIfHGngUTnV",
+        trackId: "7LSZyRFY3SzFFocAGnyJXV",
         fileName: "preview_033.mp3",
       },
       {
-        trackId: "0yhMmqax6HRAZxI7udEask",
+        trackId: "740uwCK09xVPNPr4hFpcWW",
         fileName: "preview_034.mp3",
       },
       {
-        trackId: "7AzlLxHn24DxjgQX73F9fU",
+        trackId: "5Bng1Bwy7PFQys6qByKmdT",
         fileName: "preview_035.mp3",
       },
       {
-        trackId: "3azJifCSqg9fRij2yKIbWz",
+        trackId: "0bxzF2Kyql0pQ58Mymztue",
         fileName: "preview_036.mp3",
       },
       {
-        trackId: "6MOst484piqXpzPPRRe8i5",
+        trackId: "3J07YIMTnaBv4p6QerX7hT",
         fileName: "preview_037.mp3",
       },
       {
-        trackId: "5FkoSXiJPKTNyYgALRJFhD",
+        trackId: "1Hrucz5n0cjYuPApCk8VkL",
         fileName: "preview_038.mp3",
       },
       {
-        trackId: "2t8yVaLvJ0RenpXUIAC52d",
+        trackId: "2KM7hci1ohUILFerXvGUrO",
         fileName: "preview_039.mp3",
       },
       {
-        trackId: "1fewSx2d5KIZ04wsooEBOz",
+        trackId: "6d4l6gEGNVRJTgrYzGAlpR",
         fileName: "preview_040.mp3",
       },
       {
-        trackId: "3DK6m7It6Pw857FcQftMds",
+        trackId: "5py3FJrHEuR67BjR7wm8uj",
         fileName: "preview_041.mp3",
       },
       {
-        trackId: "5hVghJ4KaYES3BFUATCYn0",
+        trackId: "6z8F2qpz1zM5ehf0tVFAPu",
         fileName: "preview_042.mp3",
       },
       {
-        trackId: "4gh0ZnHzaTMT1sDga7Ek0N",
+        trackId: "3lI1sZnyM1Ju3ApgNo0ITT",
         fileName: "preview_043.mp3",
       },
       {
-        trackId: "6vSRW8utiYAdoCfJG2v86r",
+        trackId: "3O6qgosmTPliBdsuCIOqMh",
         fileName: "preview_044.mp3",
       },
       {
-        trackId: "3iVcZ5G6tvkXZkZKlMpIUs",
-        fileName: "preview_045.mp3",
-      },
-      {
-        trackId: "1tm7c4V0kqLiN0XPVnoUcT",
+        trackId: "10uuINeqxckcD70rf1h4rg",
         fileName: "preview_046.mp3",
       },
       {
-        trackId: "2G1tXoGBaEMJ7FKGnkf6ud",
+        trackId: "7Bczytfc62eccD7kSqSnFh",
         fileName: "preview_047.mp3",
       },
       {
-        trackId: "2aPTvyE09vUCRwVvj0I8WK",
+        trackId: "3xNMMrXGcFIg06FfTVxod9",
         fileName: "preview_048.mp3",
       },
       {
-        trackId: "4jvjzW7Hm0yK4LvvE0Paz9",
+        trackId: "3KwHAXvJ3XpIFynhiB54iB",
         fileName: "preview_049.mp3",
       },
       {
-        trackId: "2qOm7ukLyHUXWyR4ZWLwxA",
+        trackId: "3ymDF1SxC4c8i0GUZN3Mg2",
         fileName: "preview_050.mp3",
       },
     ],
