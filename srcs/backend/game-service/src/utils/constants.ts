3b58de68b39805c2172cac0803bca94a9bb729ee
pacomariano28 <pacomariano28@gmail.com>
2026-08-20 13:12:54 +0200

    fix(gateway): fix jwt_secret and clean .env

--- srcs/backend/game-service/src/utils/constants.ts
+++ srcs/backend/game-service/src/utils/constants.ts
@@ -3,7 +3,7 @@ export const PLAYLIST_SERVICE_URL =
   process.env.PLAYLIST_SERVICE_URL || "http://playlist-service:4004";
 export const CONTENT_SERVICE_URL =
   process.env.CONTENT_SERVICE_URL || "http://content-service:4003";
-export const ROUND_NUMBER = 1;
+export const ROUND_NUMBER = 5;
 export const MAX_PLAYER = 5;
 export const ROUND_COUNTDOWN_SECONDS = 5;
 export const GUESS_WINDOW_SECONDS = 20;
